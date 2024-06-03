c266da5f6cdc58e25f1305dce5dd546f07b87010
j4v3d <javedrealtor@gmail.com>
2024-06-02 21:17:43 -0400

    1:36:52 Filter component

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,3 +1,5 @@
+import Filter from '@/components/Filter'
+import ProductList from '@/components/ProductList'
 import Image from 'next/image'
 import React from 'react'
 
@@ -5,7 +7,7 @@ export default function ListPage() {
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       {/* CAMPAIGN */}
-      <div className="bg-pink-50 px-4 flex justify-between">
+      <div className="hidden bg-pink-50 px-4 sm:flex justify-between h-64">
         <div className="w-2/3 flex flex-col items-center justify-center gap-8">
          <h1 className='text-4xl font-demibold leading-[48px] text-gray-700'>Grab up to 50% off on <br/>Selected Products</h1> 
          <button className='rounded-3xl bg-lama text-white w-max py-3 px-5 text-sm'>Buy Now</button>
@@ -14,6 +16,11 @@ export default function ListPage() {
           <Image src='/woman.png' alt='' fill className='object-contain'/>
         </div>
       </div>
+      {/* Filter */}
+      <Filter />
+      {/* Products */}
+      <h1 className='mt-12 text-xl font-semibold'>Shoes for You!</h1>
+      <ProductList />
     </div>
   )
 }
